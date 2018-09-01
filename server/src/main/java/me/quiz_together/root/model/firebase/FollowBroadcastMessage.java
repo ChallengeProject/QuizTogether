8343c75be131d0e5c0d629a81917bcf12c129a95
woojin.kang <woojin.kang@linecorp.com>
2018-09-01 11:29:14 +0900

    Fix typo

--- server/src/main/java/me/quiz_together/root/model/firebase/FollowBroadcastMessage.java
+++ server/src/main/java/me/quiz_together/root/model/firebase/FollowBroadcastMessage.java
@@ -3,14 +3,12 @@ package me.quiz_together.root.model.firebase;
 import lombok.Builder;
 import lombok.Getter;
 import me.quiz_together.root.support.hashid.HashBroadcastId;
-import me.quiz_together.root.support.hashid.HashUserId;
 
 @Builder
 @Getter
 public class FollowBroadcastMessage {
     @HashBroadcastId
     private Long broadcastId;
-    @HashUserId
     private String title;
     private String description;
     private String userName;
