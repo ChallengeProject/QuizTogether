9a2e13333d68321d0990e7767840cbd3a77a31f6
woojin.kang <woojin.kang@linecorp.com>
2018-11-20 23:17:49 +0900

    Add Ignore annotation

--- server/src/test/java/me/quiz_together/root/IntegrationTest.java
+++ server/src/test/java/me/quiz_together/root/IntegrationTest.java
@@ -1,5 +1,6 @@
 package me.quiz_together.root;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -8,5 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@Ignore
 public class IntegrationTest {
 }
